e91d2f1a43e3207e7dad860a8f9970910b8ecf1a
yubaolee <yubaolee@163.com>
2016-12-20 20:36:26 +0800

    v2.0

--- src/openauth.net.sql
+++ src/openauth.net.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50716
 File Encoding         : 65001
 
-Date: 2016-12-09 18:23:18
+Date: 2016-12-20 20:34:51
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -35,8 +35,27 @@ CREATE TABLE `answer` (
 -- ----------------------------
 -- Records of answer
 -- ----------------------------
-INSERT INTO `answer` VALUES ('cb19e855-ec72-4be2-a6e4-60b9c59189d6', 'dsdsdsdsdsdsdsds', '0', '0', 'cb19e855-ec72-4be2-a6e4-60b9c59189d6', '2016-12-19 19:36:32', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
-INSERT INTO `answer` VALUES ('dd19e855-cc72-4be2-a6e4-60b9c59189d6', 'dsdsdsdsdsdsdsds', '0', '0', 'cb19e855-ec72-4be2-a6e4-60b9c59189d6', '2016-12-05 19:36:26', 'f77e649d-7159-48b3-bbc1-750f83403cae', '0', '0');
+INSERT INTO `answer` VALUES ('0467300b-7bc4-4d40-b7c5-252e2606b286', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-20 20:26:50', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('066fd8ac-1c0a-4922-a027-1b19a54fa319', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-20 14:10:34', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('14fa4b4f-a01f-4cbc-be6e-5ca538183047', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-20 20:26:39', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('22f2f531-882d-42b3-b6e1-a2956aee44b7', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-20 14:08:08', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('2ca2ee8d-bca9-40a0-b767-35a33ab2b653', 'dsdsdsdsdface[左哼哼] ', '0', '0', '977be216-ecfa-43af-97ba-217d2e867e4e', '2016-12-16 15:53:47', 'f77e649d-7159-48b3-bbc1-750f83403cae', '0', '0');
+INSERT INTO `answer` VALUES ('3226ad63-8c21-4ead-8003-38eedf878276', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-20 20:27:01', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('359164f8-c58f-448f-9fdc-7f845cb53edf', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-20 20:26:45', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('4076ffb7-7cdc-43d0-ad10-ca160d4a5882', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-19 19:48:44', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('43820df3-dffc-4ee3-b98f-35df2ae06665', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-20 16:20:23', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('4d179790-5017-4d55-aa60-a5175ab84daa', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-20 20:26:57', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('53472199-25df-4cfc-8eed-9102e0d47229', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-20 14:52:18', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('7170608f-d15d-4889-92bd-f1cf27799b8a', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-19 20:35:41', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('94e43378-2d47-4b2a-a655-c2ff52b106b3', 'fdddfdd', '0', '0', '977be216-ecfa-43af-97ba-217d2e867e4e', '2016-12-14 21:01:55', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('9f0eb31f-a54d-4a1a-851a-92afa04c825a', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-20 15:20:19', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('ab269931-b58d-424e-a73a-e335f7cae7ed', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-20 15:17:08', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('ad4377f3-d8a0-45c7-a7dc-28dfa021e28b', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-19 20:36:35', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('b15448fd-eea2-48ae-a6c1-76ea3219198c', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-20 15:24:55', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('b4ad9662-d7ec-4074-a3c3-2fe1b38ab8fc', 'wewwweface[太开心] ', '0', '0', 'b767e51e-5c2c-4a8d-878f-92cec0b82052', '2016-12-19 14:12:53', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('bd7daf75-f435-4c22-9551-e6de29b966e3', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-20 17:50:00', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('d2db22ae-9a74-4418-aba7-5506f3e780c5', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-20 15:21:54', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
+INSERT INTO `answer` VALUES ('f20d2e89-cc45-42d6-8da8-45041eb24942', 'ok', '0', '0', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', '2016-12-20 08:36:00', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0');
 
 -- ----------------------------
 -- Table structure for donate
@@ -55,6 +74,31 @@ CREATE TABLE `donate` (
 -- Records of donate
 -- ----------------------------
 INSERT INTO `donate` VALUES ('998dc1b1-a870-4c5d-8853-434c9ec1a94c', '2016-11-01 13:57:12', '180', '微信', '佚名（做好事要留名，联系我，谢谢！！）');
+
+-- ----------------------------
+-- Table structure for message
+-- ----------------------------
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message` (
+  `id` varchar(50) NOT NULL,
+  `time` datetime NOT NULL,
+  `content` varchar(200) NOT NULL,
+  `href` varchar(1000) DEFAULT NULL,
+  `from` varchar(50) NOT NULL,
+  `to` varchar(50) NOT NULL,
+  `fromstatus` int(4) NOT NULL DEFAULT '0' COMMENT '来源方状态',
+  `tostatus` int(4) NOT NULL DEFAULT '0' COMMENT '到达方状态',
+  `type` varchar(20) DEFAULT NULL COMMENT '类型',
+  `fromname` varchar(50) DEFAULT NULL COMMENT '来源方名称',
+  `toname` varchar(50) DEFAULT NULL COMMENT '到达方名字',
+  `read` int(4) NOT NULL DEFAULT '0',
+  `title` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of message
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for question
@@ -81,8 +125,9 @@ CREATE TABLE `question` (
 -- ----------------------------
 -- Records of question
 -- ----------------------------
-INSERT INTO `question` VALUES ('dfdfd', 'cb19e855-ec72-4be2-a6e4-60b9c59189d6', 'fdfdfdfdfdd', '0', '0', null, '2016-12-05 15:34:40', 'd8c8a352-452f-45ea-b699-4dc566885694', '0', '0', '0', '0', '0', 'dd19e855-cc72-4be2-a6e4-60b9c59189d6');
-INSERT INTO `question` VALUES ('sdsvcccc', 'f1213640-9c92-4aa8-8cfb-641f4855b63c', 'face[太开心] dsdsdsdsdsd', '0', '0', null, '2016-12-05 14:11:01', 'yubaolee', '0', '0', '0', '0', '0', null);
+INSERT INTO `question` VALUES ('dd', '7d6f6f41-7144-417d-b5ec-bd3f6f4b9993', 'ddddddddddfffdface[互粉] ', '0', '0', null, '2016-12-19 14:04:55', 'd8c8a352-452f-45ea-b699-4dc566885694', '1', '0', '18', '0', '5', null);
+INSERT INTO `question` VALUES ('dsdsds', '977be216-ecfa-43af-97ba-217d2e867e4e', 'dsdsdsfffffff', '0', '0', null, '2016-12-14 19:26:11', 'f77e649d-7159-48b3-bbc1-750f83403cae', '58', '0', '5', '0', '5', '94e43378-2d47-4b2a-a655-c2ff52b106b3');
+INSERT INTO `question` VALUES ('dccccc', 'b767e51e-5c2c-4a8d-878f-92cec0b82052', 'cccgdgdgd', '0', '0', null, '2016-12-19 14:05:04', 'd8c8a352-452f-45ea-b699-4dc566885694', '5', '0', '1', '0', '5', null);
 
 -- ----------------------------
 -- Table structure for sysconf
@@ -156,5 +201,5 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('d8c8a352-452f-45ea-b699-4dc566885694', 'yubaolee', '盖世玉宝', 'lueSGJZetyySpUndWjMBEg==', '2', '2016-12-01 10:44:52', null, null, null, '0', '0', '0.jpg', '0', '0');
-INSERT INTO `user` VALUES ('f77e649d-7159-48b3-bbc1-750f83403cae', '123', '无敌&王子', 'lueSGJZetyySpUndWjMBEg==', '0', '2016-12-01 16:50:06', null, null, null, '0', '0', '3.jpg', '0', '0');
+INSERT INTO `user` VALUES ('d8c8a352-452f-45ea-b699-4dc566885694', 'yubaolee', '盖世玉宝', 'lueSGJZetyySpUndWjMBEg==', '2', '2016-12-01 10:44:52', null, null, null, '0', '25', '0.jpg', '1', '0');
+INSERT INTO `user` VALUES ('f77e649d-7159-48b3-bbc1-750f83403cae', 'admin', '无敌&王子', 'lueSGJZetyySpUndWjMBEg==', '0', '2016-12-01 16:50:06', null, null, null, '0', '4', '3.jpg', '0', '0');
